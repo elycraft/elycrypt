9b11099db1576df11ed9771bb21260deea8dbe79
elycraft <80060314+elycraft@users.noreply.github.com>
2021-09-19 12:11:29 +0200

    Delete build.py

--- build.py
+++
@@ -1,4 +0,0 @@
-import shutil
-
-shutil.copy("/Users/raphael/ElyPython/elycrypt/elycrypt.py","/Library/Frameworks/Python.framework/Versions/3.7/bin/elycrypt")
-shutil.copy("/Users/raphael/ElyPython/elycrypt/elycrypt.py","/Users/raphael/ElyPython/elycrypt/executable/elycrypt")
